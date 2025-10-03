0d0a4c041bf7e95a9b706bfefb3a42c3e806629e
Danielyilma <105011761+Danielyilma@users.noreply.github.com>
2025-10-03 09:03:42 +0300

    0 ms (100%) 17.9 MB (13.19%) 10-03-2025 0394-decode-string Medium .py

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,41 +1,22 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        
+        stack = []
         num = ""
-        res = []
-        i = 0
-        temp = ""
 
-        while i < len(s):
-            if s[i].isnumeric():
-                num += s[i]
-
-            elif s[i] == "[":
-                if temp:
-                    res.append(temp)
-                temp = ""
-                count = 0
-                j = i + 1
-                while j < len(s) and (s[j] != "]" or count):
-                    if s[j] == "[":
-                        count += 1
-                    
-                    if s[j] == "]":
-                        count -= 1
-                    temp += s[j]
-                    j += 1
-                i = j
-
-                if not num:
-                    num = "1"
-                res.append(int(num) * self.decodeString(temp))
+        for st in s:
+            if st == "[":
+                stack.append(num)
                 num = ""
+                continue
+            elif st == "]":
                 temp = ""
+                while not stack[-1].isnumeric():
+                    temp = stack.pop() + temp
+                num1 = int(stack.pop())
+                stack.append(temp * num1)
+            elif st.isdigit():
+                num += st
             else:
-                temp += s[i]
-            i += 1
+                stack.append(st)
         
-        if temp:
-            res.append(temp)
-        
-        return "".join(res)
+        return "".join(stack)
