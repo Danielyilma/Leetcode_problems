ac9b9986f3e82f36ed2bb7408c91db77acb1a4d3
Danielyilma <105011761+Danielyilma@users.noreply.github.com>
2025-10-19 12:01:12 +0300

    118 ms (5.25%) 28.1 MB (11.23%) 10-19-2025 0064-minimum-path-sum Medium .py

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -2,6 +2,7 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         directions = [(0, 1), (1, 0)]
         destination = (len(grid) - 1, len(grid[0]) - 1)
+        store = {}
 
         isbound = lambda row, col: 0 <= row < len(grid) and 0 <= col < len(grid[0])
 
@@ -12,13 +13,18 @@ class Solution:
             if (row, col) == destination:
                 return grid[row][col]
 
+            if (row, col) in store:
+                return store[(row, col)]
+
             for r, c in directions:
                 new_row, new_col = row + r, col + c
 
                 if not isbound(new_row, new_col):
                     continue
 
-                sums = min(sums, dfs(new_row, new_col))
+                count = dfs(new_row, new_col)
+                store[(new_row, new_col)] = count
+                sums = min(sums, count)
             
             return sums + grid[row][col]
         
