096b6802007f4959b37ed858559357bb1505f69c
Danielyilma <105011761+Danielyilma@users.noreply.github.com>
2026-08-11 12:27:19 +0300

    21 ms (66.97%) 19.9 MB (29.46%) 08-11-2026 1169-invalid-transactions Medium .py

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,23 +1,25 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        transaction_map = []
         res = []
 
         for i in range(len(transactions)):
-            name, time, amnt, city = transactions[i].split(",")
-
-            if int(amnt) > 1000:
-                res.append(transactions[i])
+            name, time, amount, city = transactions[i].split(",")
+            transaction_map.append([name, int(time), int(amount), city, i])
+        print(transaction_map)
+        for i in range(len(transaction_map)):
+            if transaction_map[i][2] > 1000:
+                res.append(transactions[transaction_map[i][4]])
                 continue
     
-            for j in range(len(transactions)):
+            for j in range(len(transaction_map)):
                 if i == j:
                     continue
-
-                name2, time2, amnt2, city2 = transactions[j].split(",")
                 
-                diff = abs(int(time2) - int(time))
-                if name == name2 and city != city2 and diff < 60:
-                    res.append(transactions[i])
+                if transaction_map[i][0] == transaction_map[j][0] and transaction_map[i][3] != transaction_map[j][3] and abs(transaction_map[i][1] - transaction_map[j][1]) <= 60:
+                    res.append(transactions[transaction_map[i][4]])
                     break
+
+            
         
         return res
