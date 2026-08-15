a90c6d8aff97d25d6efc901531479c3fed54fbe2
Danielyilma <105011761+Danielyilma@users.noreply.github.com>
2026-08-15 15:10:45 +0300

    7 ms (71.62%) 23.1 MB (38.89%) 08-15-2026 0056-merge-intervals Medium .py

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,12 +1,19 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        merge = [intervals[0]]
+
+        print(intervals)
+        start, end = intervals[0]
+        res = []
 
         for i in range(1, len(intervals)):
-            if intervals[i][0] <= merge[-1][1]:
-                merge[-1][1] = max(intervals[i][1], merge[-1][1])
+            # if there is overlap between the two adjecent indexes
+            if end >= intervals[i][0]:
+                end = max(end, intervals[i][1])
             else:
-                merge.append(intervals[i])
-        
-        return merge
+                res.append([start, end])
+                start, end = intervals[i]
+
+        res.append([start, end])
+
+        return res
